da2999a0ca2227b944f2582489316218b3ec1867
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2017-04-22 19:41:32 -0500

    Fix web build screen size

--- html/src/lando/systems/ld38/client/HtmlLauncher.java
+++ html/src/lando/systems/ld38/client/HtmlLauncher.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import lando.systems.ld38.LudumDare38;
+import lando.systems.ld38.utils.Config;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Config.gameWidth, Config.gameHeight);
         }
 
         @Override
