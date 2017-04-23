de98b140dde1dc60ec2cf3b8d859108b8dc9c34e
Doug Graham <dsgraham@gmail.com>
2017-04-22 23:53:04 -0500

    Actually use width and height in the desktoplauncher

--- desktop/src/lando/systems/ld38/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/ld38/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package lando.systems.ld38.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import lando.systems.ld38.LudumDare38;
+import lando.systems.ld38.utils.Config;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Config.gameWidth;
+		config.height = Config.gameHeight;
 		new LwjglApplication(new LudumDare38(), config);
 	}
 }
