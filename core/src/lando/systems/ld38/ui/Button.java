cf6d29329dc720bc99b5a6e484a6f50cfc557cc1
Ian McNamara <ian.mcnamara@doit.wisc.edu>
2017-04-23 16:28:47 -0500

    button update - tooltips wip

--- core/src/lando/systems/ld38/ui/Button.java
+++ core/src/lando/systems/ld38/ui/Button.java
@@ -1,18 +1,23 @@
 package lando.systems.ld38.ui;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g3d.particles.ResourceData;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import lando.systems.ld38.utils.Assets;
+import lando.systems.ld38.utils.Config;
+
+import static com.badlogic.gdx.Gdx.input;
 
 /**
  * Brian Ploeckelman created on 4/16/2016.
  */
 public class Button {
-
-//    private static final float offset = 4f;
 
     public final TextureRegion region;
     public final Rectangle bounds;
@@ -21,36 +26,19 @@ public class Button {
     private OrthographicCamera camera;
     private Vector2 touchPosScreen = new Vector2();
 
-//    public boolean active;
-//    public boolean drawNinePatch;
-
     public Button(TextureRegion region, Rectangle bounds, OrthographicCamera camera, String text, String tooltip) {
         this.bounds = new Rectangle(bounds);
         this.camera = camera;
         this.region = region;
         this.text = text;
-        this.tooltip = tooltip;
-//        this.active = false;
-//        this.drawNinePatch = true;
+        this.setTooltip(tooltip);
     }
 
     public Button(TextureRegion region, Rectangle bounds, OrthographicCamera camera) {
         this.bounds = new Rectangle(bounds);
         this.camera = camera;
         this.region = region;
-//        this.active = false;
-//        this.drawNinePatch = true;
     }
-
-//    public Button(TextureRegion region, Rectangle bounds) { // , boolean drawNinePatch) {
-//        this(region, bounds);
-//        this.drawNinePatch = drawNinePatch;
-//    }
-
-//    public boolean checkForTouch(float screenX, float screenY) {
-//        return bounds.contains(screenX,screenY);
-//    }
-
 
 
     public boolean checkForTouch(int screenX, int screenY) {
@@ -59,22 +47,44 @@ public class Button {
         return bounds.contains(touchPosScreen.x, touchPosScreen.y);
     }
 
+    private float tooltipBackgroundHeight;
+    private float tooltipBackgroundWidth;
+    private float tooltipTextOffsetY = 0f;
+    private GlyphLayout tooltipTextGlyphLayout;
+
+    private static final float TOOLTIP_TEXT_OFFSET_Y = 24f; // Catches characters with ligatures below the baseline
+    private static final float TOOLTIP_TEXT_PADDING_X = 18f; // Padding for all 4 sides around the text
+    private static final float TOOLTIP_TEXT_PADDING_Y = 4f; // Padding for all 4 sides around the text
+    private static final float TOOLTIP_TEXT_SCALE = 0.34f; // Padding for all 4 sides around the text
+    private static final float TOOLTIP_TEXT_OFFSET_X = TOOLTIP_TEXT_PADDING_X * TOOLTIP_TEXT_SCALE;
+
+    public void setTooltip(String tooltip) {
+        this.tooltip = tooltip;
+        tooltipTextGlyphLayout = new GlyphLayout(Assets.fancyFont, tooltip);
+        tooltipBackgroundHeight = tooltipTextGlyphLayout.height + (TOOLTIP_TEXT_PADDING_Y * 2) + TOOLTIP_TEXT_OFFSET_Y;
+        tooltipBackgroundWidth = tooltipTextGlyphLayout.width + (TOOLTIP_TEXT_PADDING_X * 2);
+        tooltipTextOffsetY = (tooltipTextGlyphLayout.height + TOOLTIP_TEXT_PADDING_Y + TOOLTIP_TEXT_OFFSET_Y) * TOOLTIP_TEXT_SCALE;
+    }
 
 
     public void render(SpriteBatch batch) {
-//        if (drawNinePatch) {
-//            if (active) Assets.selectedNinepatch.draw(batch,
-//                                                      bounds.x - offset,
-//                                                      bounds.y - offset,
-//                                                      bounds.width + 2f * offset,
-//                                                      bounds.height + 2f * offset);
-//            else        Assets.transparentNinepatch.draw(batch,
-//                                                      bounds.x - offset,
-//                                                      bounds.y - offset,
-//                                                      bounds.width + 2f * offset,
-//                                                      bounds.height + 2f * offset);
-//        }
+        batch.setColor(Color.RED);
         batch.draw(region, bounds.x, bounds.y, bounds.width, bounds.height);
+        batch.setColor(Color.WHITE);
+
+        if (tooltip != null && checkForTouch(input.getX(), input.getY())) {
+            float tX = input.getX();
+            float tY = Config.gameHeight - input.getY();
+            Assets.woodPanel.draw(batch, tX, tY, tooltipBackgroundWidth * TOOLTIP_TEXT_SCALE, tooltipBackgroundHeight * TOOLTIP_TEXT_SCALE);
+            Assets.drawString(batch,
+                    tooltip,
+                    tX + TOOLTIP_TEXT_OFFSET_X,
+                    tY + tooltipTextOffsetY,
+                    Color.WHITE,
+                    TOOLTIP_TEXT_SCALE,
+                    Assets.fancyFont);
+        }
+
     }
 
 }
